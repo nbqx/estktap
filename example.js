7796901ded9546056a7e542f2e5c22a488338209
Bruno Herfst <hello@brunoherfst.com>
2018-04-10 17:12:46 +1000

    [Bugfix] Define a target array globally
    As arrays are passed by reference we where popping array from caller.
    Fixed! Also simplifies tests to use global variable. The readme states
    clearly that targets are optional.

--- example.js
+++ example.js
@@ -1,25 +1,14 @@
 var test = require('tape'),
     estktap = require(__dirname);
 
-test('extendscript tap test',function(t){
-  estktap('this must be true',__dirname+'/test/fixtures/test/test.jsx',true);
-  t.end();
-});
+var targets = ['indesign-12'];
 
-test('extendscript tap test with target',function(t){
-  estktap('this must be true',__dirname+'/test/fixtures/test/test.jsx',true,['indesign-13']);
+test('extendscript tap test',function(t){
+  estktap('this must be true',__dirname+'/test/fixtures/test/test.jsx',true,targets);
   t.end();
 });
 
 test('extendscript tap test with function',function(t){
-  estktap('deepEqual',__dirname+'/test/fixtures/test/obj.jsx',function(d){
-    t.deepEqual(d,{x:0, y:0, width:3000, height: 5000});
-  });
-  t.end();
-});
-
-test('extendscript tap test with multiple targets and function',function(t){
-  var targets = ['indesign-11','indesign-13'];
   estktap('deepEqual',__dirname+'/test/fixtures/test/obj.jsx',function(d){
     t.deepEqual(d,{x:0, y:0, width:3000, height: 5000});
   },targets);
